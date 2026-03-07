ce9f70408046ce88e54bc5447d273ef237df8154
shaherm2025-a11y <shaherm2025-a11y@gmail.com>
2026-03-08 02:57:11 +0300

    alter notifications open and close2

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'screens/expert_home_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
     FlutterLocalNotificationsPlugin();
